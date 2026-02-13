21010bf6ce9d123334fba9a6370318a3f7330f88
lzhao2708 <lily.zhao2708@gmail.com>
2026-02-13 14:47:47 -0600

    Created printInventory method

--- src/GroceryManagementSystem.java
+++ src/GroceryManagementSystem.java
@@ -5,5 +5,19 @@ public Class GroceryManagementSystem {
         int[] itemStocks = new int[10];
     }
 
-
+    /**
+     * Prints the inventory of the grocery store. Excludes slots that are empty (null names).
+     * 
+     * @param names  Array of item names.
+     * @param prices Array of item prices.
+     * @param stocks Array of item stock quantities.
+     */
+    public static void printInventory(String[] names, double[] prices, int[] stocks) {
+        System.out.println("Inventory:");
+        for (int i = 0; i < names.length; i++) {
+            if (names[i] != null) {
+                System.out.println(names[i] + " - Price: $" + prices[i] + ", Stock: " + stocks[i]);
+            }
+        }
+    }
 }
